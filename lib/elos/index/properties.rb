0eda57772c390b83b01202e17c4bedf99e3607a2
Tetsuri Moriya <tetsuri.moriya@gmail.com>
2015-05-31 15:23:12 +0900

    version 1.0.10

--- lib/elos/index/properties.rb
+++ lib/elos/index/properties.rb
@@ -2,6 +2,26 @@ module Elos::Index::Properties
   extend ActiveSupport::Concern
 
   class_methods do
+    def array_property(value_type)
+      {
+        properties: {
+          value: send("#{value_type}_property")
+        }
+      }
+    end
+
+    def string_property
+      { type: 'string', index: 'not_analyzed' }
+    end
+
+    def integer_property
+      { type: 'integer' }
+    end
+
+    def long_property
+      { type: 'long' }
+    end
+
     def boolean_property
       { type: 'boolean' }
     end
--- lib/elos/version.rb
+++ lib/elos/version.rb
@@ -1,3 +1,3 @@
 class Elos
-  VERSION = '1.0.9'
+  VERSION = '1.0.10'
 end
