a2a667420f9453a799aa5059026c0ecf6e0688d8
Tetsuri Moriya <tetsuri.moriya@gmail.com>
2015-06-07 01:36:25 +0900

    version 1.0.15

--- spec/support/sample_app.rb
+++ spec/support/sample_app.rb
@@ -1,21 +1,18 @@
 Dir[File.expand_path('../../sample_app/**/*\.rb', __FILE__).to_s].each { |f| require(f) }
 
-classes = [Entry, EEntryRepo, EEntry, AEntry]
-
-classes.each do |klass|
-  %i(find search).each do |method|
-    klass.singleton_class.send(:alias_method, "_#{method}".to_sym, method)
-    klass.define_singleton_method(method) do |*args|
-      klass.refresh
-      klass.send("_#{method}", *args)
+RSpec.configure do |config|
+  config.prepend_before(:suite) do
+    Elos.index_classes.each do |klass|
+      %i(find search).each do |method|
+        klass.singleton_class.send(:alias_method, "_#{method}".to_sym, method)
+        klass.define_singleton_method(method) do |*args|
+          klass.refresh
+          klass.send("_#{method}", *args)
+        end
+      end
     end
   end
-end
-
-RSpec.configure do |config|
   config.prepend_before do
-    classes.each do |klass|
-      klass.unindex
-    end
+    Elos.unindex
   end
 end
